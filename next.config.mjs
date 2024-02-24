a7a077563b04715e9b15cab6bbe897084c6d3feb
shashank <shash@gmail>
2024-02-24 12:11:26 +0530

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images:{
+        domains:['utfs.io'],
+        remotePatterns:[{
+            protocol:'https',
+            hostname:'utfs.io',
+            port:''
+
+
+
+        }
+
+        ]
+    }
+};
+
+export default nextConfig;
